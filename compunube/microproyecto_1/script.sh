d7a1b4fbc1986d2f114016f19278d075c5ac5380
Daniel Alexander Muelas Rivera <danielmuelasrivera483@gmail.com>
2022-10-18 21:20:41 -0500

    cambios 2

--- compunube/microproyecto_1/script.sh
+++ compunube/microproyecto_1/script.sh
@@ -33,14 +33,3 @@ wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install consul -y
 consul -v
-
-
-echo "************************************"
-echo "*       Instalación de LXD         *"
-echo "************************************"
-sudo apt-get install lxd -y
-newgrp lxd
-lxd init --auto
-lxc launch ubuntu:18.04 web1
-lxc launch ubuntu:18.04 web2
-lxc launch ubuntu:18.04 web3
